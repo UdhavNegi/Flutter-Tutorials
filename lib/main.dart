4c52243beff2f964ba142f3dc775f09b860df5c7
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-12 14:19:56 +0530

    ListTile completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
-void main()
-{
-  runApp(FlutterApp());
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
 }
 
-class FlutterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title : "FlutterApp",
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
       ),
-      home : DashBoardScreen()
-
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-
 }
 
-class DashBoardScreen extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// This is the homepage for our flutter app
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var arrNames = ['Raman', 'Ramanujan', 'Rajesh', "James", 'John', 'Rahim', 'Ram', ];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title : Text("Dashboard"),
-      ),
-
-      body : Container(
-        color : Colors.blueGrey,
-        margin : EdgeInsets.all(11),
-        child: Padding(
-          padding: const EdgeInsets.only(top : 21, left : 34, bottom : 11, right : 34),
-          child: Text('Hello world!', style : TextStyle(fontSize: 25, color : Colors.white),),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('Hello'),
         ),
-      )
-    );
+        body: ListView.separated(itemBuilder: (context, index){
+            return ListTile(
+              leading: Text('${index+1}'),
+              title: Text(arrNames[index]),
+              subtitle: Text('Number'),
+              trailing: Icon(Icons.add),
+            );
+          },
+        itemCount: arrNames.length,
+        separatorBuilder: (context, index){
+          return Divider(height: 20, thickness: 1,);
+        },
+        )
+        );
   }
 }
-
