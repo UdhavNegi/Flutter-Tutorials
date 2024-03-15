a70c165e5643cc096f3f0130af8f96e570c50344
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-15 11:05:43 +0530

    updating correctly with stateful widgets completed

--- lib/main.dart
+++ lib/main.dart
@@ -56,11 +56,14 @@ class MyHomeState extends State<MyHomePage>{
   var no1Controller = TextEditingController();
   var no2Controller = TextEditingController();
 
+  var result = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar : AppBar(
         title : Text('Basic Calculations'),
+        backgroundColor: Colors.blue,
       ),
 
       body : Container(
@@ -72,22 +75,66 @@ class MyHomeState extends State<MyHomePage>{
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextField(
+                  keyboardType: TextInputType.number,
                   controller: no1Controller,
                 ),
             
             
                 TextField(
+                  keyboardType: TextInputType.number,
                   controller: no2Controller,
                 ),
             
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(onPressed: (){}, child: Text('Add')),
-                    ElevatedButton(onPressed: (){}, child: Text('Sub')),
-                    ElevatedButton(onPressed: (){}, child: Text('Mult')),
-                    ElevatedButton(onPressed: (){}, child: Text('Div')),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(21.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var sum = no1 + no2;
+                        result = "The sum of $no1 and $no2 is $sum";
+                        setState(() {
+                          
+                        });
+                      }, 
+                      child: Text('Add'), 
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                      ),
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var diff = no1 - no2;
+                        result = "The difference between $no1 and $no2 is $diff";
+                        setState(() {});
+                      },
+                      child: Text('Sub'), 
+                      ),
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var mul = no1 * no2;
+                        result = "The Producrt of $no1 and $no2 is $mul";
+                        setState(() {});
+                      }, child: Text('Mult')),
+                      ElevatedButton(onPressed: (){
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+
+                        var div = no1 / no2;
+                        result = "The division of $no1 by $no2 is $div";
+                        setState(() {});
+                      }, child: Text('Div')),
+                    ],
+                  ),
+                ),
+
+                Padding(padding: EdgeInsets.all(21), 
+                        child : Text(result, style : TextStyle(fontSize: 25))
                 )
               ],
             ),
