f099820c95ed4f663c6a6b1fd10d87b5dcfd414d
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-15 16:43:34 +0530

    completed splash screen in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,57 @@
-// import 'package:first_app/my_profile_screen.dart.dart';
+// import 'package:first_app/IntroPage.dart';
 // import 'package:first_app/Widgets/rounded_btn.dart';
-// import 'package:first_app/splash_screen.dart';
 // import 'package:first_app/ui_helper/util.dart';
-// import 'package:flutter/cupertino.dart';
+import 'package:first_app/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart';
 
-// import 'my_profile_screen.dart.dart';
+// import './IntroPage.dart';
 
 
 
 void main() {
-  runApp(FlutterApp());
+  runApp(const MyApp());
 }
 
-class FlutterApp extends StatelessWidget {
-  // const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
             primarySwatch: Colors.blue
             ),
-            home: DashBoardScreen(),
+            home: SplashScreen(),
     );
   }
 }
 
-// This is created by me 
 class DashBoardScreen extends StatelessWidget{
+
+  var fun = () => print("inside dash board screen");
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title : Text('Classico'),
-        backgroundColor: Colors.orange,
+        title : Text('Dashboard'),
       ),
-      body : Text('Hello world'),
-  
+      body : Center(
+        child: Container(
+          child : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('DashBoard Screen')
+            ],
+          )
+        ),
+      )
     );
   }
 }
-
