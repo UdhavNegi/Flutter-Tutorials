e5a6caa5fd08bb8b72e9376f78aba915dea43535
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-16 17:14:10 +0530

    AnimatedCrossFade() widgets done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -31,47 +32,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  bool isFirst = true;
   var myOpacity = 1.0;
-  var isVisisble = true;
+  // var isVisisble = false;
+
+  @override
+  void initState()
+  {
+    super.initState();
+  }
+
+  void reload()
+  {
+    setState(() {
+      isFirst = !isFirst;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Animated Opacity"),
+        title: Text("Foo Animation"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedOpacity(
-            opacity: myOpacity,
-            duration: Duration(seconds: 4), 
-            curve : Curves.elasticIn,
-            child : Container(
-              width : 200, 
-              height : 100,
-              color : Colors.blue,
-            ), ), 
-            ElevatedButton(onPressed: (){
-              myOpacity = 0.0;
-              setState(() {
+            AnimatedCrossFade(
+              duration: Duration(
+                seconds : 4,
+              ),
+              firstChild: Container(
+                width : 200, 
+                height : 200, 
+                color : Colors.grey.shade400
+              ),
+            
+              secondChild: Container(
+                width : 200, 
+                height : 200, 
+                color : Colors.red.shade400
+              ),
 
-                if(isVisisble)
-                {
-                  myOpacity = 0.0; 
-                  isVisisble = false;// invisible 
-                }
-                else 
-                {
-                  myOpacity = 1.0;
-                  isVisisble = true;
-                }
+              // sizeCurve: Curves.fastOutSlowIn,
+              firstCurve: Curves.easeInOut,
+              secondCurve: Curves.bounceInOut,
+
+
+              crossFadeState: isFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond, 
+
+            
+            ),
+            ElevatedButton(onPressed: (){
+              // isFirst = !isFirst;
+              setState(() {
+                reload();
               });
 
-            }, child: Text('Close'))
+            }, child: Text('Show')) 
           ],
         ),
       )
