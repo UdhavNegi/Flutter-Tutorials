649c99f18806a1c183962cd96e50e9b191483fdb
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-16 15:02:31 +0530

    Animated Opacity widget done

--- lib/main.dart
+++ lib/main.dart
@@ -31,62 +31,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _width = 200.0;
-  var _height = 100.0;
-  bool flag = true;
-  Color bgColor = Colors.blueGrey;
-
-  Decoration myDecor = BoxDecoration(
-    borderRadius: BorderRadius.circular(2), 
-    color : Colors.blueGrey,
-  );
+  
+  var myOpacity = 1.0;
+  var isVisisble = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Animated Opacity"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedContainer(
-              width : _width, 
-              height : _height,
-              curve : Curves.bounceInOut,
-              decoration: myDecor,
-              duration: Duration(seconds: 2), 
-            ), 
+            AnimatedOpacity(
+            opacity: myOpacity,
+            duration: Duration(seconds: 4), 
+            curve : Curves.elasticIn,
+            child : Container(
+              width : 200, 
+              height : 100,
+              color : Colors.blue,
+            ), ), 
             ElevatedButton(onPressed: (){
+              myOpacity = 0.0;
               setState(() {
-                if(flag)
+
+                if(isVisisble)
                 {
-                  _width = 100.0;
-                  _height = 200.0;
-                  myDecor = BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color : Colors.orange,
-                  );
-                  flag = false;
+                  myOpacity = 0.0; 
+                  isVisisble = false;// invisible 
                 }
                 else 
                 {
-                  _width = 200.0;
-                  _height = 100.0;
-                  myDecor = BoxDecoration( 
-                    borderRadius: BorderRadius.circular(2),
-                    color : Colors.blueGrey,
-                  );
-                  flag = true;
+                  myOpacity = 1.0;
+                  isVisisble = true;
                 }
               });
 
-            }, child: Text('Animate'))
+            }, child: Text('Close'))
           ],
-        )
-      ), 
+        ),
+      )
     );
   }
 }
+ 
