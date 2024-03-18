e6e9bafeb83fd8d56591ca19abb639e8f99f6346
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-18 10:52:55 +0530

    ListWheelScrollView() done

--- lib/main.dart
+++ lib/main.dart
@@ -32,67 +32,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isFirst = true;
-  var myOpacity = 1.0;
-  // var isVisisble = false;
-
-  @override
-  void initState()
-  {
-    super.initState();
-  }
-
-  void reload()
-  {
-    setState(() {
-      isFirst = !isFirst;
-    });
-  }
+  var arrIndex = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Foo Animation"),
+        backgroundColor: Colors.blue,
+        title: Text("3D List"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedCrossFade(
-              duration: Duration(
-                seconds : 4,
+        child: ListWheelScrollView(
+          itemExtent: 100,
+          children: arrIndex.map((value) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child : Center(child: Text('$value', style : TextStyle(fontSize: 21, color : Colors.white))),
+              decoration: BoxDecoration(
+              color : Colors.blue, 
+              borderRadius: BorderRadius.circular(10),
+
               ),
-              firstChild: Container(
-                width : 200, 
-                height : 200, 
-                color : Colors.grey.shade400
-              ),
-            
-              secondChild: Container(
-                width : 200, 
-                height : 200, 
-                color : Colors.red.shade400
-              ),
-
-              // sizeCurve: Curves.fastOutSlowIn,
-              firstCurve: Curves.easeInOut,
-              secondCurve: Curves.bounceInOut,
-
-
-              crossFadeState: isFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond, 
-
-            
+              width : double.infinity, 
             ),
-            ElevatedButton(onPressed: (){
-              // isFirst = !isFirst;
-              setState(() {
-                reload();
-              });
-
-            }, child: Text('Show')) 
-          ],
+          )).toList(), 
+          
         ),
       )
     );
