f7c320e03a64e43722e994ded5e77e83e09d368d
UdhavNegi <159622491+UdhavNegi@users.noreply.github.com>
2024-03-19 16:28:53 +0530

    fetch api calls for get request done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:first_app/detail_page.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Shared Preferences'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -32,37 +33,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  List<dynamic> users = [];
+  
+  void initState()
+  {
+    super.initState();
+    getData();
+    print("users is $users");
   }
 
   @override
   Widget build(BuildContext context) {
+    var arrNames = ["first", "second", "third"];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Container(
-        child : Center(
-          child : InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage()));
-            },
-            child: Hero(
-              tag : 'background',
-              child : Image.asset('assets/images/logo.jpg', width : 150, 
-              height: 100,)
-            ),
-          )
+      body :  ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index){
+          final user = users[index];
+          final email = user["email"];
+          return ListTile(
+            leading: CircleAvatar(child: Text("${(index+1).toString()}")),
+            title: Text(users[index]["name"]["first"]),
+            subtitle: Text(users[index]["email"]),
+            trailing: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(users[index]["picture"]["thumbnail"])
+              ),
+          );
+        }
         )
-      ) 
     );
   }
   
-
+  void getData() async {
+    final uri = Uri.parse('https://randomuser.me/api/?results=100');
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json["results"];
+    });
+  }
 }
